2f5a31bf706c1b060473cbab31127aa6d93c8c00
cmkimber <cmkimber@gmail.com>
2024-07-30 14:00:50 -0400

    add collapsable help text to pane 2

--- bikeshare-to-app/app_pane2_draft2.R
+++ bikeshare-to-app/app_pane2_draft2.R
@@ -40,9 +40,16 @@ ui <- page_fluid(
   titlePanel(
     "Starting vs. Ending Station Use by Month"
   ),
-  layout_columns(
-    card(helpText("Lorem ipsum etc")),
-    col_widths = c(6, -6)
+  accordion(
+    open = FALSE,
+    accordion_panel(
+      "CLick for Help",
+      layout_columns(
+        card(helpText("Explore the most popular stations to start and end Bike Share Toronto trips at during 2022 using this pane. The controls below can be used to select full year or monthly data, choose the month of interest, or search for a particular station. The tick box to display the top 15 stations will restrict the maps to showing the most popular stations; these 15 stations match those shown in the tables below, which can be expanded or hidden.")),
+        card(helpText("A station can also be selected by clicking a point in the map or row in a table. The slopegraph at the bottom of the page compares the relative popularity of stations for starting or ending trips in the selected time period. The slopegraph shows all stations which are in the top 15 in popularity for either starting or ending trips during the time period selected. A station can also be selected by clicking on the slopegraph itself.")),
+        col_widths = c(6, 6)
+      )
+    )
   ),
   layout_columns(
     card(radioButtons("year_vs_month",
